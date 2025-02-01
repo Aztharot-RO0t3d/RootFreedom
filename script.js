fec366ea7a1abd5c0711a843c9d7a1808f062736
Leonardo Ernesto Correa Hernández <aztharot.ru@proton.me>
2025-01-31 19:11:02 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const title = document.getElementById("title");
+    const text = "Bienvenido a Aztharot - Servicios en Ciberseguridad y Programación";
+    title.textContent = "";
+
+    let index = 0;
+    function typeWriter() {
+        if (index < text.length) {
+            title.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(typeWriter, 100);
+        } else {
+            title.classList.add("text-blink");
+        }
+    }
+    typeWriter();
+});
